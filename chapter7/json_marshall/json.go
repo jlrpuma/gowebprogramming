a005e5a30eb2879544665f4e0f6bf66eee74ac6a
Jose Luis <jlrp39@gmail.com>
2021-12-16 14:10:10 -0500

    Fix json go file (Marshall example)

--- chapter7/json_marshall/json.go
+++ chapter7/json_marshall/json.go
@@ -1,1 +1,55 @@
-{"id":1,"content":"Hello Go","author":{"id":1,"name":"Jose"},"comments":[{"id":1,"content":"Hello Jose","author":""},{"id":2,"content":"hau?","author":""}]}
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	post := Post{
+		Id:      1,
+		Content: "Hello Go",
+		Author: Author{
+			Id:   1,
+			Name: "Jose",
+		},
+		Comments: []Comment{
+			{
+				Id:      1,
+				Content: "Hello Jose",
+			},
+			{
+				Id:      2,
+				Content: "hau?",
+			}},
+	}
+
+	jsonContent, err := json.Marshal(post)
+	if err != nil {
+		fmt.Println("Error on marshall", err)
+	}
+	err = ioutil.WriteFile("post.json", jsonContent, 0644)
+	if err != nil {
+		fmt.Println()
+	}
+
+}
