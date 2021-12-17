f05ed6239c7d0e001a1a12bf3239b15e3ba95ac2
Jose Luis <jlrp39@gmail.com>
2021-12-17 14:40:43 -0500

    Update method on data, added.
    Update method was completed.

--- chapter7/web_service/data.go
+++ chapter7/web_service/data.go
@@ -50,6 +50,7 @@ func (post *Post) create() (err error) {
 	return
 }
 
-func (pot *Post) update() (err error) {
-
+func (post *Post) update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
 }
